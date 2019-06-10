5a49bc67f20cdd41e4943260f83c2be11e3448d6
Martin Hediger <ma.hed@bluewin.ch>
2019-06-10 13:52:40 +0200

    Polls

--- urls.py
+++ urls.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import url, include
+from django.contrib import admin
 from aldryn_django.utils import i18n_patterns
 import aldryn_addons.urls
 
 
 urlpatterns = [
+
     # add your own patterns here
+    url(r'^polls/', include('polls.urls')),
+    url(r'^admin/', admin.site.urls),
 ] + aldryn_addons.urls.patterns() + i18n_patterns(
     # add your own i18n patterns here
     *aldryn_addons.urls.i18n_patterns()  # MUST be the last entry!
