b2c976651f11d7b305a536df0cc19e1e8089d04c
Martin Hediger <ma.hed@bluewin.ch>
2019-06-10 17:27:07 +0200

    update

--- polls/models.py
+++ polls/models.py
@@ -13,6 +13,7 @@ class Question(models.Model):
 
 
 
+
 class Choice(models.Model):
     question    = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
