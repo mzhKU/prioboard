3061c84dac013fd9884173be32c8e3aff4b9a3a0
Martin Hediger <ma.hed@bluewin.ch>
2019-06-10 15:03:59 +0200

    db

--- settings.py
+++ settings.py
@@ -11,9 +11,18 @@ INSTALLED_ADDONS = [
 import aldryn_addons.settings
 aldryn_addons.settings.load(locals())
 
+# add your project specific apps here
+# INSTALLED_APPS = []
 
 # all django settings can be altered here
-
+#print(INSTALLED_APPS)
+print("hoi_fabian" + str(INSTALLED_APPS))
 INSTALLED_APPS.extend([
-    # add your project specific apps here
+    'polls.apps.PollsConfig',
+    # 'django.contrib.admin',
+    # 'django.contrib.auth',
+    # 'django.contrib.contenttypes',
+    # 'django.contrib.sessions',
+    # 'django.contrib.messages',
+    # 'django.contrib.staticfiles',
 ])
